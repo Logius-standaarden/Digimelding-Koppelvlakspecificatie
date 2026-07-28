e418b725ed15f8fb8fe316952811decf2da1099c
Alexander Green <alexander.green@logius.nl>
2026-07-28 11:33:16 +0200

    Verduidelijking tijdzone (#14)
    * Verduidelijking tijdzone (zie #13)
    
    * Geen permalink naar 1.4
    
    * StUF-pagina's

--- js/config.mjs
+++ js/config.mjs
@@ -6,12 +6,7 @@ loadRespecWithConfiguration({
         "name" : "Peter Haasnoot",
         "url" : "https://www.logius.nl"
       } ],
-  editors: [ 
-      { 
-        "company" : "Logius",
-        "name" : "Edwin Wisse",
-        "url" : "https://www.logius.nl"
-      },
+  editors: [
       { 
         "company" : "Logius",
         "name" : "Peter Haasnoot",
@@ -19,14 +14,12 @@ loadRespecWithConfiguration({
       }
     ],
   github: "https://github.com/Logius-standaarden/Digimelding-Koppelvlakspecificatie",
-  nl_markdownTableClass: "simple",
-  previousPublishDate: "2021-01-31",
-  previousPublishVersion: "1.3",
+  previousPublishDate: "2021-06-01",
+  prevVersion: [],
   pubDomain: "digimelding",
-  publishDate: "2021-02-19",
-  publishVersion: "1.4",
+  publishDate: "2026-07-20",
+  publishVersion: "1.4.1",
   shortName: "koppelvlak",
   specStatus: "WV",
-  specType: "HR",
-  subtitle: "Handreiking",
+  specType: "HR"
 });
