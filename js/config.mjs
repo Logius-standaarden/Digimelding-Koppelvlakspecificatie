825cca175fbb1808e6768de34a8292b9c3007887
Alexander Green <alexander.green@logius.nl>
2026-07-28 11:17:08 +0200

    Herpubliceer 1.4.0 voor status en permalink

--- js/config.mjs
+++ js/config.mjs
@@ -19,14 +19,11 @@ loadRespecWithConfiguration({
       }
     ],
   github: "https://github.com/Logius-standaarden/Digimelding-Koppelvlakspecificatie",
-  nl_markdownTableClass: "simple",
-  previousPublishDate: "2021-01-31",
-  previousPublishVersion: "1.3",
   pubDomain: "digimelding",
   publishDate: "2021-02-19",
-  publishVersion: "1.4",
+  publishVersion: "1.4.0",
   shortName: "koppelvlak",
-  specStatus: "WV",
+  specStatus: "DEF",
   specType: "HR",
   subtitle: "Handreiking",
 });
